f3d09c63965eb8e54fb29c3ca40e1d501a872175
KIM YUEN <kim_man@live.com>
2024-06-20 11:48:08 +1000

    create ListGroup component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import Message from './Message';
+import ListGroup from './components/ListGroup';
 
 function App() {
+  let items = ['New York', 'London', 'Tokyo', 'Paris', 'Los Angeles'];
+  let moreItems = ['Berlin', 'Sydney', 'Rome', 'Moscow', 'Toronto'];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <div>
-      <Message />
+      <ListGroup
+        items={items}
+        heading="cities"
+        onSelectItem={handleSelectItem}
+      />
+      <ListGroup
+        items={moreItems}
+        heading="more cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
